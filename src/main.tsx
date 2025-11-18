501cbca9da3c43cebda6155f223c0e751fc5092d
Dylan Ryan Schmidt <dylanschmidt1246@gmail.com>
2025-11-18 13:21:54 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
-import { inject } from '@vercel/analytics';
-inject();
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Preview from './Preview';
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Preview />
+    <Analytics />
   </React.StrictMode>
 );
